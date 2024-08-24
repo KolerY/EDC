8499d01f28f603451e1e40dbfa15ab5c58236479
Yannick Blanchette <yannickblanchette@hotmail.com>
2024-08-24 12:54:18 -0400

    Changement de cache

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = "static-cache-v46";
+const CACHE_NAME = "static-cache-v47";
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
   "offline.html",
